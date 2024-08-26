8751841b3cc5264c7d645d76d064d9881cf1dbf1
Danzel Serrano <danzel.serrano@gmail.com>
2024-08-25 22:27:27 -0400

    started email module

--- notipy/email/__main__.py
+++ notipy/email/__main__.py
@@ -6,28 +6,36 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 parser = argparse.ArgumentParser(
-    prog = 'notipy',
+    prog = 'notipy.email',
     description = ''
+)
+parser.add_argument(
+    '--source',
+    required=True
+)
+parser.add_argument(
+    '--destination',
+    required=True
 )
 parser.add_argument(
     '--gmail_app_pass',
     required=True
 )
 parser.add_argument(
-    'params',
+    'pipe_params',
     nargs="*"
 )
 
 args = parser.parse_args()
 if not sys.stdin.isatty():
-    args.params.extend(sys.stdin.read().splitlines())
+    args.pipe_params.extend(sys.stdin.read().splitlines())
 
 GMAIL_APP_PASS = args.gmail_app_pass
 
 msg = MIMEMultipart('alternative')
 msg['Subject'] = "[notipy] - Process Complete"
-msg['From']    = 'danzel.serrano@gmail.com'
-msg['To']      = 'danzel.serrano@gmail.com'
+msg['From']    = args.source
+msg['To']      = args.destination
 
 text = "Process Complete."
 html = f"""
@@ -37,7 +45,7 @@ html = f"""
             <div>
                 <h1>[notipy] - Process Complete</h1>
                 <div>
-                    {'<br>'.join(ast.literal_eval(repr(args.params)))}
+                    {'<br>'.join(ast.literal_eval(repr(args.pipe_params)))}
                 </div>
             </div>
         </body>
@@ -49,12 +57,11 @@ msg.attach(MIMEText(html, 'html'))
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
     smtp_server.login(
-        'danzel.serrano@gmail.com',
+        args.source,
         GMAIL_APP_PASS
     )
     smtp_server.sendmail(
-        'danzel.serrano@gmail.com',
-        'danzel.serrano@gmail.com',
+        args.source,
+        args.destination,
         msg.as_string()
     )
-
